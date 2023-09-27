a75b98cf6d6e764e8245ec929c14c08e61e81118
Michael Myers <michael.myers@wustl.edu>
2023-09-26 21:16:33 -0500

    Changed arg for pad, trim from Vector to AbstractVector

--- src/surfaces.jl
+++ src/surfaces.jl
@@ -135,7 +135,7 @@ collapse(inds::Union{AbstractRange, Vector}, surf::SurfaceSpace) =
 	return filter(x -> x != 0, surf.remap[CollapseMW][inds])
 
 "Grow `x` to `size(surf, Inclusive())` by padding it with zeros along the medial wall"
-function pad(x::Union{AbstractRange, Vector}, surf::SurfaceSpace)
+function pad(x::Union{AbstractRange, AbstractVector}, surf::SurfaceSpace)
 	length(x) == size(surf, Exclusive()) || 
 		error("Input length must match the size of the surface, exclusive of medial wall")
 	out = zeros(eltype(x), size(surf, Inclusive()))
