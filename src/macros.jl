033e17c7350e152cdb14adb8bed9983ec260ab34
Michael Myers <michael.myers@wustl.edu>
2023-10-19 16:53:18 -0500

    Had forgotten to add macros.jl

---
+++ src/macros.jl
@@ -0,0 +1,14 @@
+
+export @exclusive, @collapse
+
+macro exclusive(expr)
+	surface = expr.args[1]
+	k = expr.args[2]
+	return :($surface[$k, Exclusive()])
+end
+
+macro collapse(expr)
+	return :(collapse($expr, $(esc(expr.args[2]))))
+end
+
+
