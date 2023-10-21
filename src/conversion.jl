c179b9dd2a36697eaff700919a75753069fb6736
Michael Myers <michael.myers@wustl.edu>
2023-10-21 12:27:32 -0500

    Fixed conversion to Mesh

--- src/conversion.jl
+++ src/conversion.jl
@@ -5,9 +5,9 @@ function GeometryBasics.Mesh(hem::Hemisphere)
 	!isnothing(hem.triangles) || error("triangle component must be defined")
 	triangles = hem.triangles
 	coords = coordinates(hem)
-	pts = [GeometryBasics.Point{3, Float32}(coords[v, :]) for v in 1:size(hem)]
+	pts = [GeometryBasics.Point{3, Float32}(coords[:, v]) for v in 1:size(hem)]
 	triangles = [
-		GeometryBasics.TriangleFace(triangles[v, :]) for v in 1:size(triangles, 1)
+		GeometryBasics.TriangleFace(triangles[:, v]) for v in 1:size(triangles, 2)
 	]
 	return GeometryBasics.Mesh(pts, triangles)
 end
