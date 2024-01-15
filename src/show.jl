c20f6fec9517df2b7f3caf627a0fdd40e830c6ec
Michael Myers <michael.myers@wustl.edu>
2024-01-14 18:22:51 -0600

    Updated show.jl

--- src/show.jl
+++ src/show.jl
@@ -1,6 +1,7 @@
 
 function Base.show(io::IO, ::MIME"text/plain", h::Hemisphere)
-	print("Hemisphere with $(size(h)) vertices ($(size(h, Exclusive())) without medial wall)")
+	print("Hemisphere ($(brainstructure(h))) with $(size(h)) vertices ")
+	print("$(size(h, Exclusive())) without medial wall)")
 end
 
 function Base.show(io::IO, ::MIME"text/plain", c::CorticalSurface)
