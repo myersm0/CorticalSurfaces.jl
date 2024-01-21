a77ebbef3819c19723e860d460abba9ad4fe4709
Michael Myers <michael.myers@wustl.edu>
2024-01-21 09:07:49 -0600

    Cleaned up accessors.jl implementations and docstrings

--- src/accessors.jl
+++ src/accessors.jl
@@ -1,7 +1,26 @@
 
+"""
+    brainstructure(surf)
+
+Get the `BrainStructure` designation (`CORTEX_LEFT` or `CORTEX_RIGHT`) of a
+`Hemisphere` struct `surf`, or, in the event that `surf` is a `CorticalSurface`,
+a `Vector` of both its left and right hemisphere labels.
+"""
+function brainstructure(surf::SurfaceSpace) end
+
 brainstructure(h::Hemisphere) = h.label
 
 brainstructure(c::CorticalSurface) = LR
+
+
+"""
+    medial_wall(s)
+
+Get the medial wall `BitVector` from a `SurfaceSpace` struct, where `true` denotes
+medial wall membership, `false` otherwise.
+"""
+medial_wall(s::SurfaceSpace) = s.medial_wall
+
 
 """
     getindex(c, h)
@@ -9,51 +28,54 @@ brainstructure(c::CorticalSurface) = LR
 Index into the `L` or `R` `Hemisphere` of a `CorticalSurface`
 """
 function Base.getindex(c::CorticalSurface, h::BrainStructure)
-	haskey(c.hems, h) || throw(KeyError)
 	return c.hems[h]
 end
 
 """
-	 getindex(hem, s, Inclusive())
+	 getindex(surf, s, Inclusive())
 
-Access supplementary spatial data `s` for a `Hemisphere`, inclusive of medial wall
+Access supplementary spatial data `s::Symbol` for a `SurfaceSpace`. Optionally pass 
+a `MedialWallIndexing` trait `Inclusive()` or `Exclusive()` to inform handling 
+of medial wall (default is `Inclusive()`).
 """
+function Base.getindex(surf::SurfaceSpace, s::Symbol, ::MedialWallIndexing) end
+
+function Base.getindex(surf::SurfaceSpace, s::Symbol)
+	return getindex(surf, s, Inclusive())
+end
+
 function Base.getindex(hem::Hemisphere, s::Symbol, ::Inclusive)
 	haskey(hem.appendix, s) || throw(KeyError)
 	return hem.appendix[s].data[Inclusive()]
 end
 
-"""
-	 getindex(hem, s, Exclusive())
-
-Access supplementary spatial data `s` for a `Hemisphere`, exclusive of medial wall
-"""
 function Base.getindex(hem::Hemisphere, s::Symbol, ::Exclusive)
 	haskey(hem.appendix, s) || throw(KeyError)
 	return hem.appendix[s].data[Exclusive()]
 end
 
-Base.getindex(hem::Hemisphere, s::Symbol) =
-	return getindex(hem, s, Inclusive())
-
-"Index into a `Hemisphere`'s supplementary spatial data"
-Base.getindex(x::SpatialData, mw::MedialWallIndexing, args...) =
-	return getindex(x.data[mw], args...)
-
-function Base.getindex(c::CorticalSurface, s::Symbol, args...)
+function Base.getindex(c::CorticalSurface, s::Symbol, mw::MedialWallIndexing)
 	haskey(c[L].appendix, s) && haskey(c[R].appendix, s) || throw(KeyError)
-	a = @views c[L][s, args...]
-	b = @views c[R][s, args...]
+	a = @views c[L][s, mw]
+	b = @views c[R][s, mw]
 	style = DataStyle(a)
 	DataStyle(b) == style || error("SpatialData structs must have the same DataStyle")
 	return cat(style, a, b)
 end
 
+# to be used only as a helper for public-facing getindex functions
+function Base.getindex(x::SpatialData, mw::MedialWallIndexing, args...)
+	return getindex(x.data[mw], args...)
+end
+
+
+## cat functions to be used internally as helpers in combining coordinates 
+## and SpatialData across hemispheres
+
 Base.cat(::IsSquare, a::AbstractMatrix, b::AbstractMatrix) =
 	return cat(a, b; dims = 1:2)
 
-# TODO: take a closer look at the performance of this one;
-# what's going on?
+# TODO: take a closer look at the performance of this one; what's going on?
 function Base.cat(::IsSquare, a::SparseMatrixCSC, b::SparseMatrixCSC)
 	size_a = size(a)
 	size_b = size(b)
@@ -69,87 +91,105 @@ Base.cat(::IsScalarList, a::Vector, b::Vector) =
 Base.cat(::IsNestedList, a::Vector, b::Vector) =
 	return [a; [x .+ size(a, 1) for x in b]]
 
-"""
-    size(hem, mw)
 
-Get the number of vertices of a `Hemisphere`, `Exclusive()` or `Inclusive()` of medial wall
 """
+    size(surf)
+
+Get the number of vertices of a `SurfaceSpace`, inclusive of medial wall.
+"""
+Base.size(surf::SurfaceSpace)  = size(surf, Inclusive())
+
+"""
+    size(surf, mw)
+
+Get the number of vertices of a `SurfaceSpace`, `Exclusive()` or `Inclusive()` 
+of medial wall.
+"""
+function Base.size(surf::SurfaceSpace, mw::MedialWallIndexing) end
+
 Base.size(hem::Hemisphere, mw::MedialWallIndexing) = hem.size[mw]
 
-"Get the number of vertices of a `Hemisphere`, `Inclusive()` of medial wall"
-Base.size(hem::Hemisphere) = size(hem, Inclusive())
+Base.size(c::CorticalSurface, mw::MedialWallIndexing) = size(c[L], mw) + size(c[R], mw)
 
-"Get the number of vertices of a `CorticalSurface`"
-Base.size(c::CorticalSurface, args...) = size(c[L], args...) + size(c[R], args...)
 
-"Get coordinates from a `SurfaceSpace`, `Exclusive()` or `Inclusive()` of medial wall"
-coordinates(s::SurfaceSpace, mw::MedialWallIndexing) = s.coordinates[mw]
+"""
+    coordinates(surf)
 
-"Get coordinates from a `Hemisphere`, `Inclusive()` of medial wall"
+Get coordinates from a `SurfaceSpace`, inclusive of medial wall. Each column
+of the output represents a vertex, and the rows represent x, y, and z.
+"""
 coordinates(s::SurfaceSpace) = coordinates(s, Inclusive())
+
+"""
+    coordinates(surf)
+
+Get coordinates from a `SurfaceSpace`, `Inclusive()` or `Exclusive()` of medial wall. 
+Each column of the output represents a vertex, and the rows represent x, y, and z.
+"""
+coordinates(s::SurfaceSpace, mw::MedialWallIndexing) = s.coordinates[mw]
 
 "Get coordinates from a Vector of Hemispheres"
 coordinates(v::Vector{Hemisphere}, args...) = 
 	hcat([coordinates(h, args...) for h in v]...)
 
-"Get vertex numbers from a `Hemisphere`, `Inclusive()` of medial wall"
-vertices(hem::Hemisphere) = hem.vertices[(Ipsilateral(), Inclusive())]
 
-"Get vertex numbers from a `Hemisphere`, `Exclusive()` of medial wall"
-vertices(hem::Hemisphere, ::Exclusive) = hem.vertices[(Ipsilateral(), Exclusive())]
+"""
+    vertices(s)
 
-"Get vertex numbers from a `Hemisphere`, `Exclusive()` of medial wall"
+Get vertex numbers from a `SurfaceSpace` struct, inclusive of medial wall.
+"""
+vertices(s::SurfaceSpace) = vertices(s, Inclusive())
+
+"""
+    vertices(s, mw)
+
+Get vertex numbers from a `SurfaceSpace` struct, `Inclusive()` or `Exclusive()`
+of medial wall.
+"""
+function vertices(s::SurfaceSpace, mw::MedialWallIndexing) end
+
+vertices(hem::Hemisphere, mw::MedialWallIndexing) = hem.vertices[(Ipsilateral(), mw)]
+
 vertices(hem::Hemisphere, args...) = hem.vertices[args...]
 
-"Get vertex numbers from a `CorticalSurface`, `Inclusive()` of medial wall"
-vertices(c::CorticalSurface) = c.vertices[Inclusive()]
-
-"Get vertex numbers from a `CorticalSurface`, `Exclusive()` or `Inclusive()` of medial wall"
 vertices(c::CorticalSurface, mw::MedialWallIndexing) = c.vertices[mw]
 
-"Get the medial wall `BitVector` from a `Hemisphere` or `CorticalSurface`"
-medial_wall(s::SurfaceSpace) = s.medial_wall
 
 """
-    keys(hem)
+    keys(surf)
 
-Get the names of the supplementary data elements, if any, that exist for a `Hemisphere`
+Get the names of the supplementary data elements, if any, that exist for a `SurfaceSpace`
 """
+function Base.keys(surf::SurfaceSpace) end
+
 Base.keys(hem::Hemisphere) = keys(hem.appendix)
 
-"""
-    haskey(hem, k)
-
-Check whether a `Hemisphere` has the symbol `k` among its supplementary spatial data
-"""
-Base.haskey(hem::Hemisphere, k::Symbol) = haskey(hem.appendix, k)
-
-"""
-    keys(c)
-
-Get the names of the supplementary data that exist for both hemispheres 
-of a `CorticalSurface`
-"""
 Base.keys(c::CorticalSurface) = intersect(keys(c[L]), keys(c[R]))
 
-"""
-    haskey(c, k)
 
-Check whether a `CorticalSurface` has the symbol `k` among its supplementary spatial data
 """
+    haskey(surf, k)
+
+Check whether a `SurfaceSpace` has the symbol `k` among its supplementary spatial data
+"""
+function Base.haskey(surf::SurfaceSpace, k::Symbol) end
+
+Base.haskey(hem::Hemisphere, k::Symbol) = haskey(hem.appendix, k)
+
 Base.haskey(c::CorticalSurface, k::Symbol) = k in keys(c)
 
-"""
-    matches_topology(c1, c2)
 
-Check whether `SurfaceSpace`s `c1` and `c2` share the same vertex space, medial
+"""
+    node_correspondence(s1, s2)
+
+Check whether `SurfaceSpace`s `s1` and `s2` share the same vertex space, medial
 wall definition, and topology.
 """
-function matches_topology(c1::SurfaceSpace, c2::SurfaceSpace)
+function node_correspondence(s1::SurfaceSpace, s2::SurfaceSpace)
 	return false
 end
 
-function matches_topology(c1::CorticalSurface, c2::CorticalSurface)
+function node_correspondence(c1::CorticalSurface, c2::CorticalSurface)
 	size(c1) == size(c2) || return false
 	medial_wall(c1) == medial_wall(c2) || return false
 	for hem in LR
@@ -160,12 +200,12 @@ function matches_topology(c1::CorticalSurface, c2::CorticalSurface)
 	return true
 end
 
-function matches_topology(c1::Hemisphere, c2::Hemisphere)
-	size(c1) == size(c2) || return false
-	medial_wall(c1) == medial_wall(c2) || return false
-	(!isnothing(c1.triangles) && !isnothing(c2.triangles)) ||
+function node_correspondence(h1::Hemisphere, h2::Hemisphere)
+	size(h1) == size(h2) || return false
+	medial_wall(h1) == medial_wall(h2) || return false
+	(!isnothing(h1.triangles) && !isnothing(h2.triangles)) ||
 		error("Can't compare topology unless both surfaces have `triangles` defined")
-	c1.triangles == c2.triangles || return false
+	h1.triangles == h2.triangles || return false
 	return true
 end
 
